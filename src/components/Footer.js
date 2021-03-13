40c5d2c98f5a5f9847383109941c04eb5c6af77f
Ely Kahn <ely.kahn@gmail.com>
2021-03-12 23:56:47 -0600

    Update Footer.js
    Personalized footer info

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,16 +3,16 @@ import './Footer.css'
 
 export default () => (
   <div>
-    <h2 className="taCenter">
-      Follow us{' '}
-      <a href="https://instagram.com/thrivegoldcoast/">@thrivegoldcoast</a>
+    <h3 className="taCenter">
+      Follow me{' '}
+      <a href="https://www.instagram.com/creativ_ely_io/">insta</a>
     </h2>
     <br />
     <footer className="footer">
       <div className="container taCenter">
         <span>
           © Copyright {new Date().getFullYear()} All rights reserved. Crafted by{' '}
-          <a href="https://thriveweb.com.au/">Thrive</a>.
+          <a href="https://ely.io/">ely kahn</a>.
         </span>
       </div>
     </footer>
